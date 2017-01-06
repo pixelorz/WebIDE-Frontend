3281ffee16dda2ca255d95ee3d3a4fbc37350a9f
pixelorz <pixelorz@users.noreply.github.com>
2017-01-07 00:42:01 +0800

    Create  index.js

---
+++ app/ index.js
@@ -0,0 +1,14 @@
+/* @flow weak */
+import React from 'react'
+import { render } from 'react-dom'
+import qs from 'querystring'
+import Root from './containers/Root.jsx'
+import './styles/main.styl'
+import config from './config'
+const {spaceKey} = qs.parse(window.location.hash.slice(1))
+if (spaceKey) {
+  window.isSpaceKeySet = true
+  config.spaceKey = spaceKey
+}
+const app = React.createElement(Root)
+render(app, document.getElementById('root'))
