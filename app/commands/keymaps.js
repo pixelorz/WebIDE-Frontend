671a87638def922a18289ac7f3fc9cf7e750012b
pixelorz <pixelorz@users.noreply.github.com>
2017-01-07 01:53:17 +0800

    Create keymaps.js

---
+++ app/commands/keymaps.js
@@ -0,0 +1,16 @@
+/* @flow weak */
+// Unavailable shortcuts: shift / ctrl + (q|n|w|t|↹)
+export default {
+  'cmd+s': 'file:save',
+  'cmd+shift+c': 'git:commit',
+  'esc': 'modal:dismiss',
+  'cmd+shift+p': 'global:command_palette',
+  'cmd+alt+1':       'editor:split_pane_1',
+  'cmd+alt+shift+1': 'editor:split_pane_1',
+  'cmd+alt+2':       'editor:split_pane_vertical_2',
+  'cmd+alt+shift+2': 'editor:split_pane_horizontal_2',
+  'cmd+alt+3':       'editor:split_pane_vertical_3',
+  'cmd+alt+shift+3': 'editor:split_pane_horizontal_3',
+  'cmd+alt+4':       'editor:split_pane_vertical_4',
+  // 'cmd+p': 'global:find_file',
+}
