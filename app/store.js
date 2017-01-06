eb010d89630ea3d309bcbd4a25a4abeb84a74948
pixelorz <pixelorz@users.noreply.github.com>
2017-01-07 00:41:00 +0800

    Create store.js

---
+++ app/store.js
@@ -0,0 +1,36 @@
+/* @flow weak */
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import { composeReducers } from './utils'
+import thunkMiddleware from 'redux-thunk'
+import PanelReducer from './components/Panel/reducer'
+import PaneReducer, { PaneCrossReducer } from './components/Pane/reducer'
+import TabReducer from './components/Tab/reducer'
+import EditorReducer from './components/AceEditor/reducer'
+import FileTreeReducer from './components/FileTree/reducer'
+import ModalsReducer from './components/Modal/reducer'
+import NotificationReducer from './components/Notification/reducer'
+import TerminalReducer from './components/Terminal/reducer'
+import GitReducer from './components/Git/reducer'
+import WorkspaceReducer from './components/Workspace/reducer'
+import DragAndDropReducer from './components/DragAndDrop/reducer'
+const combinedReducers = combineReducers({
+  FileTreeState: FileTreeReducer,
+  PanelState: PanelReducer,
+  PaneState: PaneReducer,
+  TabState: TabReducer,
+  EditorState: EditorReducer,
+  ModalState: ModalsReducer,
+  TerminalState: TerminalReducer,
+  GitState: GitReducer,
+  NotificationState: NotificationReducer,
+  WorkspaceState: WorkspaceReducer,
+  DragAndDrop: DragAndDropReducer
+})
+const crossReducers = composeReducers(PaneCrossReducer)
+const finalReducer = composeReducers(crossReducers, combinedReducers)
+// const store = createStore(finalReducer, compose(applyMiddleware(thunkMiddleware), window.devToolsExtension ? window.devToolsExtension() : f => f))
+const store = createStore(finalReducer, applyMiddleware(thunkMiddleware))
+window.getState = store.getState
+export default store
+export const getState = store.getState
+export const dispatch = store.dispatch
